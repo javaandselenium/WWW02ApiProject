576c6f000f5b6da6ba35ead3f15355fe51a99d24
QSP <QSP@QSP>
2021-07-22 20:43:05 +0530

    Demo

---
+++ src/test/java/com/wipro/rmgyantra/scripts/Test1.java
@@ -0,0 +1,12 @@
+package com.wipro.rmgyantra.scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello APITesting",true);
+	}
+
+}
